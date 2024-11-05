40c88416e737e82e0747f5c5a00466a5631eb56d
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-05 22:09:37 +0100

    GenericInterface

---
+++ src/generics/interfaces/C03_GenericInterfaceImplString.java
@@ -0,0 +1,15 @@
+package generics.interfaces;
+
+//2. SECENEK Generic Interface in hangi deger olduguna karar verilir
+
+public class C03_GenericInterfaceImplString implements C01_GenericInterface<String>{  // T : generic ifade
+    @Override
+    public void print(String value) {
+
+    }
+
+    @Override
+    public String find() {
+        return null;
+    }
+}
