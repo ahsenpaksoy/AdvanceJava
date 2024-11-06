b6c913815c10348b9f94a0f4d6adbcecafc339b1
ahsenpaksoy <ahsenpaksoy@gmail.com>
2024-11-06 21:53:26 +0100

    update

--- src/enumTypes/Runner.java
+++ src/enumTypes/Runner.java
@@ -7,19 +7,25 @@ package enumTypes;
 //HIGH : Tebrikler, şifre gücünüz yüksek seviyededir:)
 
 public class Runner {
-
     public static void main(String[] args) {
-
         //String str="high";//
         //String str2="HIGH";
         //System.out.println(str.toUpperCase());
         //System.out.println(str2.equals(str.toUpperCase()));//false
+        printMesage("Low");
+        printMesage("MEDIUM");
+        printMesage("HIGH");
+
+        //---------------------------------------------
+        printMessageEnum(PasswordStrengthEnum.LOW);
+        printMessageEnum(PasswordStrengthEnum.MEDIUM);
+        printMessageEnum(PasswordStrengthEnum.HIGH);
+        printMessageEnum(PasswordStrengthEnum.MEDIUM);
 
     }
 
     //kullanıcıya paramterde verilen password gücüne gore
     //mesaj gonderen bir method olusturalım
-
     public static void printMesage(String strength){
         if (strength.equals(PasswordStrength.LOW)){
             System.out.println("Dikkat şifreniz yetersiz seviyededir!!!");
@@ -30,12 +36,26 @@ public class Runner {
         }
     }
 
+
     //kullanıcıya parametre olarak enum type ile verilen password gucune gore
     //mesaj gonderen bir method olusturalım
 
+    public static void printMessageEnum(PasswordStrengthEnum strength){//Low HIGH MEDIUM
+
+        if (strength.equals(PasswordStrengthEnum.LOW)){//0'di
+            //if (strength.ordinal()==1){
+            System.out.println("Dikkat şifreniz yetersiz seviyededir!!!");
+        } else if (strength.equals(PasswordStrengthEnum.MEDIUM)) {
+            System.out.println("Şifrenizin güçü orta sevidedir.");
+        }else if (strength.equals(PasswordStrengthEnum.HIGH)){
+            System.out.println("Tebrikler, şifre gücünüz yüksek seviyededir☺");
+
+        }
+        System.out.println("Enum Sırası : "+strength.ordinal());//ordinal methodu sıralama işlemi sağlar
+        System.out.println("Enum ismi : "+strength.name());//
+    }
+
 }
-
-
 
 
 
